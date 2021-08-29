52bfdd566c7749d9749e6471ba8b0a8a92dfdb95
José Castro <60018036+jecdegois@users.noreply.github.com>
2021-08-29 11:58:32 -0400

    Add files via upload

---
+++ progra-101/seccion-4.1/app.js
@@ -0,0 +1,17 @@
+//Crear algoritmo que basado en la nota
+//imprima si el alumno aprueba o no la materia
+// aprueba con 60
+//si tiene nota menor a 50 imprimir
+// "por favor estudie mas"
+
+let nota = 60;
+
+if (nota >= 60) {
+  console.log("El alumno esta aprobado");
+} else if (nota < 50) {
+  console.log("Por favor estudie mas");
+} else {
+  console.log("El alumno no aprueba");
+}
+
+console.log("Fin del programa");
